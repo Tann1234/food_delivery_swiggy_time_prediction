de5498986fcbe134888ca4d8a583071f898d426e
Tannu Gupta <guptatannu538@gmail.com>
2026-09-23 11:34:29 +0530

    add workflow-ci-cd

---
+++ scripts/promote_model_to_prod.py
@@ -0,0 +1,45 @@
+import mlflow
+import dagshub
+import json
+from mlflow import MlflowClient
+
+dagshub.init(
+    repo_owner="guptatannu538",
+    repo_name="food_delivery_swiggy_time_prediction",
+    mlflow=True,
+)
+
+# set the mlflow tracking server
+mlflow.set_tracking_uri(
+    "https://dagshub.com/guptatannu538/food_delivery_swiggy_time_prediction.mlflow"
+)
+
+def load_model_information(file_path):
+    with open(file_path) as f:
+        run_info=json.load(f)
+    return run_info
+
+# get the name
+model_name=load_model_information('run_information.json')['model_name']
+stage='Staging'
+
+# get the latest version from staging stage
+client=MlflowClient()
+
+# get the latest version of model in staging
+latest_versions=client.get_latest_versions(name=model_name, stages=[stage])
+
+latest_model_version_staging=latest_versions[0].version
+
+# promotion stage
+promotion_stage = 'Production'
+
+client.transition_model_version_stage(
+    name=model_name,
+    version=latest_model_version_staging,
+    stage=promotion_stage,
+    archive_existiong_versions=True
+)
+
+
+
